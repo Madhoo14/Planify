5b4c4dec496f8cc6920ea7336c7e6e886ad835cb
Madhoo14 <89360388+Madhoo14@users.noreply.github.com>
2021-12-03 16:55:50 -0500

    initial set up

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>My Planify project is going to be awesome!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
